487c61b3f32a3c596dfdd8d3e8b06ca72901fb5b
福島翔 <sho@MacBook-Pro.local>
2020-05-11 23:16:56 +0900

    nasubi

--- objeckt.rb
+++ objeckt.rb
@@ -8,25 +8,10 @@ class TASK
 end
 
 
-task1 = TASK.new(1,"ボケナス1","ボケナスビを食べる1")
-task2 = TASK.new(2,"ボケナス2","ボケナスビを食べる2")
-task3 = TASK.new(3,"ボケナス3","ボケナスビを食べる3")
+task1 = TASK.new(1,"ナス1","ナスビを食べる1")
+task2 = TASK.new(2,"ナス2","ナスビを食べる2")
+task3 = TASK.new(3,"ナス3","ナスビを食べる3")
 
 puts task1.id
 puts task2.name
 puts task3.kuwa
-
-
-
-
-
-require 'google/apis/youtube_v3'
-
-youtube = Google::Apis::YoutubeV3::YouTubeService.new
-
-youtube.key = "APIキー"
-
-
-youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: "プログラミング", max_results: 10)
-
-puts youtube_search_list
